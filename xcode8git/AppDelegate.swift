8dfba5fe2ba0392f0e080feabf0bc6a616da4899
Venkatesh Peddi <venkatesh.peddi@resmed.com>
2017-02-19 14:59:25 -0800

    Initial Commit

---
+++ xcode8git/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  xcode8git
+//
+//  Created by Venkatesh Peddi on 2/19/17.
+//  Copyright © 2017 Venkatesh Peddi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ xcode8git/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  xcode8git
+//
+//  Created by Venkatesh Peddi on 2/19/17.
+//  Copyright © 2017 Venkatesh Peddi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
